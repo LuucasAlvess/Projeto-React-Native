61ee24b40473125e20b8bd8f7524eb159907d154
LuucasAlvess <lucaslima2011h@gmail.com>
2023-02-17 22:38:42 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+      source={{ uri: 'https://cdn4.buysellads.net/uu/1/127419/1670531697-AdobeTeams.jpg'}}
+      style={styles.image}
+      />
+      <Text style={styles.text}>Para assistir os melhores filmes crie uma conta agora mesmo.</Text>
+      <Text style={styles.text2}>Please fill up email and password to login your account.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'red',
   },
+  text: {
+    color: 'red',
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  text2: {
+    color: 'aqua',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 20,
+  }
 });
